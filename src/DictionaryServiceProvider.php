83bc76d27cfe95125ac14d9e0926b8c793c08d2e
Nikita <nikita.s.kalitin@gmail.com>
2020-05-23 20:04:44 +0300

    add():composer.json, DictionaryServiceProvider

---
+++ src/DictionaryServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BlackChaose\Dictionary;
+
+use Illuminate\Support\ServiceProvider;
+
+class DictionaryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+
+        $this->app->make('BlackChaose\Dictionary\Controllers\DictionaryController');
+        $this->app->make('BlackChaose\Dictionary\Models\Dictionary');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/Migrations');
+        //fixme:
+        $this->loadViewsFrom(__DIR__.'/Views/dictionary', 'BlackChaose\Dictionary');
+        $this->publishes([
+            __DIR__.'/Views/dictionary' => resource_path('/views/vendor/dictionary'),
+        ]);
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::resource('dictionary', 'BlackChaose\Dictionary\Controllers\DictionaryController');
