722076586a8b7c3b398b8558714d885c5f334e20
Nikita <nikita.s.kalitin@gmail.com>
2020-05-25 21:08:09 +0300

    add(migrations)

---
+++ src/Migrations/2020_05_19_170044_create_dictionaries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDictionariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dictionaries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('entity');
+            $table->text('value');
+            $table->text('transcription')->nullable();
+            $table->text('type')->nullable();
+            $table->text('lang');
+            $table->bigInteger('group_id')->nullable(); //id группы для заучивания
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dictionaries');
+    }
+}
