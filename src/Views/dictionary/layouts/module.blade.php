15ecc6877809786a5019e7e4d38cc39f9788fa42
Nikita <nikita.s.kalitin@gmail.com>
2020-05-29 19:44:03 +0300

    add(Views)

---
+++ src/Views/dictionary/layouts/module.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="ru-RU">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    @yield('head')
+    @yield('styles')
+</head>
+<body>
+<div class="container">
+    <div id="module">
+
+    </div>
+</div>
+@yield('scripts')
+</body>
+</html>
